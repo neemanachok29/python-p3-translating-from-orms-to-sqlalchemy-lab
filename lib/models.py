2809746e341f3de56878f2a453a275677dd896ce
Neema Nachok <neemanachok29@gmail.com>
2023-09-12 13:45:25 +0300

    done

--- lib/models.py
+++ lib/models.py
@@ -2,9 +2,7 @@
 
 from sqlalchemy import (Column, String, Integer)
 from sqlalchemy.ext.declarative import declarative_base
-
 Base = declarative_base()
-
 class Dog(Base):
     __tablename__ = 'dogs'
 
